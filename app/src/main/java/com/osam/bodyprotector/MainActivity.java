9203778f59b6806d6dd384c60dc91be3cb432ecb
Kim Yu Hyun <youhk40@naver.com>
2020-10-09 12:59:36 +0900

    authorization system

--- app/src/main/java/com/osam/bodyprotector/MainActivity.java
+++ app/src/main/java/com/osam/bodyprotector/MainActivity.java
@@ -1,14 +1,67 @@
 package com.osam.bodyprotector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et_id;
+    private EditText et_pw;
+    private Button bt_ok;
+    private Button bt_mkid;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_id = (EditText)findViewById(R.id.editText_ID);
+        et_pw = (EditText)findViewById(R.id.editText_pw);
+        bt_ok = (Button)findViewById(R.id.bt_ok);
+        bt_mkid = (Button)findViewById(R.id.bt_mkid);
+
+        firebaseAuth = firebaseAuth.getInstance();
+        bt_ok.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = et_id.getText().toString().trim();
+                String pw = et_pw.getText().toString().trim();
+
+                if(email.isEmpty() || pw.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
+                }
+                else firebaseAuth.signInWithEmailAndPassword(email,pw).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        bt_mkid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/osam/bodyprotector/SignUpActivity.java
+++ app/src/main/java/com/osam/bodyprotector/SignUpActivity.java
@@ -1,14 +1,64 @@
 package com.osam.bodyprotector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private EditText et_id;
+    private EditText et_pw;
+    private EditText et_pwr;
+    private Button bt_signup;
+    FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        et_id = (EditText)findViewById(R.id.et_id);
+        et_pw = (EditText)findViewById(R.id.et_pw);
+        et_pwr = (EditText)findViewById(R.id.et_pwr);
+        bt_signup = (Button)findViewById(R.id.bt_signup);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        bt_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String sign_id = et_id.getText().toString().trim();
+                final String sign_pw = et_pw.getText().toString().trim();
+                final String sign_pwr = et_pwr.getText().toString().trim();
+
+                if(sign_pw.equals(sign_pwr)){
+                    firebaseAuth.createUserWithEmailAndPassword(sign_id,sign_pw).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(SignUpActivity.this, "Sign up failed : check template", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else{
+                    Toast.makeText(SignUpActivity.this, "Sign up failed : check password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
